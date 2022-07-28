971856ddce0ed043f7c294e3f9b5831f5fc37acf
Loskir <kl@sinker.ru>
2022-07-28 18:23:04 +0300

    Disable local storage

--- features/category/sort.ts
+++ features/category/sort.ts
@@ -1,10 +1,10 @@
-import connectLocalStorage from "effector-localstorage"
+// import connectLocalStorage from "effector-localstorage"
 import { combine, createEvent, createStore, restore, sample } from "effector-next"
 import { SortOrder, SortType } from "../../api"
-import { pageLoaded } from "./common"
+// import { pageLoaded } from "./common"
 
-const sortOrderLS = connectLocalStorage("sortOrder")
-const sortTypeLS = connectLocalStorage("sortType")
+// const sortOrderLS = connectLocalStorage("sortOrder")
+// const sortTypeLS = connectLocalStorage("sortType")
 
 const sortChanged = createEvent<SortType>()
 const sortOrderFlipped = createEvent()
@@ -12,19 +12,19 @@ const sortOrderFlipped = createEvent()
 const $sortOrder = createStore<SortOrder>(SortOrder.asc)
 const $sortType = restore(sortChanged, SortType.name)
 
-sample({
-  clock: pageLoaded,
-  fn: () => sortOrderLS.init(SortOrder.asc),
-  target: $sortOrder,
-})
-sample({
-  clock: pageLoaded,
-  fn: () => sortTypeLS.init(SortType.name),
-  target: $sortType,
-})
+// sample({
+//   clock: pageLoaded,
+//   fn: () => sortOrderLS.init(SortOrder.asc),
+//   target: $sortOrder,
+// })
+// sample({
+//   clock: pageLoaded,
+//   fn: () => sortTypeLS.init(SortType.name),
+//   target: $sortType,
+// })
 
-$sortOrder.updates.watch(sortOrderLS)
-$sortType.updates.watch(sortTypeLS)
+// $sortOrder.updates.watch(sortOrderLS)
+// $sortType.updates.watch(sortTypeLS)
 
 sample({
   clock: sortOrderFlipped,
