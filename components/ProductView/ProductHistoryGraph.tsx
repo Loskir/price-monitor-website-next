44e24577fe88db67f22bb13b21f12f5470a413a2
Loskir <kl@sinker.ru>
2022-09-04 20:57:36 +0300

    Refactor product price graph

--- components/ProductView/ProductHistoryGraph.tsx
+++ components/ProductView/ProductHistoryGraph.tsx
@@ -4,31 +4,32 @@ import { Line } from "react-chartjs-2"
 import colors from "tailwindcss/colors"
 import { ProductPriceModel } from "../../models/Product"
 import "chartjs-adapter-luxon"
-
-const priceColor = colors.green[500]
-const basePriceColor = colors.gray[300]
+import { DateTime } from "luxon"
 
 export const ProductHistoryGraph: React.FC<{ history: ProductPriceModel[] }> = (props) => {
   Chart.register(...registerables)
   const chartData = useMemo((): ChartData<"line"> => {
-    const labels = props.history.map((v) => new Date(v.time))
+    const labels = props.history.map((v) => DateTime.fromISO(v.time).startOf("day").toJSDate())
     const data = props.history.map((v) => v.price)
+    const dataBasePrice = props.history.map((v) => v.basePrice)
     return {
       labels,
       datasets: [{
         label: "Цена",
-        borderColor: priceColor,
-        pointBorderColor: priceColor,
-        backgroundColor: priceColor,
+        borderColor: colors.green[500],
+        pointBorderColor: colors.green[400],
+        backgroundColor: colors.green[400],
         data,
         cubicInterpolationMode: "monotone",
+        pointRadius: 1,
       }, {
         label: "Цена без скидки",
-        borderColor: basePriceColor,
-        pointBorderColor: basePriceColor,
-        backgroundColor: basePriceColor,
-        data: props.history.map((v) => v.basePrice),
+        borderColor: colors.gray[300],
+        pointBorderColor: colors.gray[200],
+        backgroundColor: colors.gray[200],
+        data: dataBasePrice,
         cubicInterpolationMode: "monotone",
+        pointRadius: 1,
       }],
     }
   }, [props.history])
@@ -44,6 +45,24 @@ export const ProductHistoryGraph: React.FC<{ history: ProductPriceModel[] }> = (
           y: {
             ticks: {
               callback: (v: any) => `${v}₽`,
+            },
+          },
+        },
+        interaction: {
+          mode: "index",
+          intersect: false,
+        },
+        plugins: {
+          tooltip: {
+            position: "nearest",
+            callbacks: {
+              title: (ctx) => {
+                const date = DateTime.fromMillis(ctx[0].parsed.x).setLocale("ru")
+                if (date.year === DateTime.now().year) {
+                  return date.toFormat("d MMMM")
+                }
+                return date.toFormat("d MMMM yyyy")
+              },
             },
           },
         },
