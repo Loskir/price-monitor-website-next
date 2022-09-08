de518a336a20d854812469c41756a0373c07a3af
Loskir <kl@sinker.ru>
2022-09-08 13:24:51 +0300

    Remove links from main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,6 @@ import Link from "next/link"
 const Home: NextPage = () => {
   return (
     <main>
-      <p>
-        <Link href={"/scanner"}>Scanner</Link>
-      </p>
-      <p>
-        <Link href={"/search"}>Search</Link>
-      </p>
-      <p>
-        <Link href={"/categories"}>Categories</Link>
-      </p>
     </main>
   )
 }
