93f5b8ac32d44f1f4cd6032db21f573ac7672f8f
Loskir <kl@sinker.ru>
2022-11-30 01:28:16 +0300

    Fix empty item height

--- components/ProductListItemNew.tsx
+++ components/ProductListItemNew.tsx
@@ -26,87 +26,89 @@ const BigPrice: React.FC<{ isMulti?: boolean; price: number }> = ({ isMulti = fa
 export const ProductListItemNew: React.FC<{ product: ProductWithPriceModel }> = ({ product }) => {
   const uom = formatUom(product)
   return (
-    <div className="pl-4 pt-4 flex relative product-list-item">
-      <div className="mr-4 rounded-2xl w-16 h-16 shrink-0 relative">
-        {product.photoUrl && (
-          <img
-            className={clsx(
-              "absolute inset-0 max-h-full m-auto",
-              styles.image,
-            )}
-            src={product.photoUrl}
-            alt="Photo"
-          />
-        )}
-      </div>
-      <div className="grow">
-        <div className="mr-4">
-          <Link href={`/product/${product.productId}`}>
-            <a className={cx("inline-block", styles.title, styles.link)}>{insertNbspIntoName(product.name)}</a>
-          </Link>
-          {/*<div className="mt-3">*/}
-          {/*  {product.shops.map((price) => (*/}
-          {/*    <div className="mt-3" key={price.shopType}>*/}
-          {/*      <div className="flex">*/}
-          {/*        <div className="">*/}
-          {/*          <BigPrice price={price.price} />*/}
-          {/*          <div className="mt-1 flex flex-wrap justify-end text-secondary text-xs">*/}
-          {/*            {price.unitPrice && (*/}
-          {/*              <span>*/}
-          {/*                {formatPrice(price.unitPrice)} ₽ за {uom}*/}
-          {/*              </span>*/}
-          {/*            )}*/}
-          {/*          </div>*/}
-          {/*        </div>*/}
-          {/*        <div className="grow" />*/}
-          {/*        <ShopLogo shopType={price.shopType} className="h-8" monochrome />*/}
-          {/*      </div>*/}
-          {/*    </div>*/}
-          {/*  ))}*/}
-          {/*</div>*/}
-          {/*<div className="mt-3">*/}
-          {/*  {product.shops.map((price) => (*/}
-          {/*    <div className="mt-1" key={price.shopType}>*/}
-          {/*      <div className="flex items-center">*/}
-          {/*        <div className="mr-4">*/}
-          {/*          <BigPrice price={price.price} />*/}
-          {/*        </div>*/}
-          {/*        <div className="mt-1 flex flex-wrap justify-end text-secondary text-xs">*/}
-          {/*          {price.unitPrice && (*/}
-          {/*            <span>*/}
-          {/*              {formatPrice(price.unitPrice)} ₽ за {uom}*/}
-          {/*            </span>*/}
-          {/*          )}*/}
-          {/*        </div>*/}
-
-          {/*        <div className="grow" />*/}
-          {/*        <ShopLogo shopType={price.shopType} className="h-8" monochrome />*/}
-          {/*      </div>*/}
-          {/*    </div>*/}
-          {/*  ))}*/}
-          {/*</div>*/}
-          {product.shops.length > 0 && (
-            <div className="mt-4">
-              <div className="flex items-center">
-                <BigPrice isMulti={product.shops.length > 1} price={product.shops[0].price} />
-                <div className="grow" />
-                <ShopLogo shopType={product.shops[0].shopType} className="h-8" monochrome />
-              </div>
-              <div className="flex flex-wrap justify-end text-secondary text-xs">
-                {product.shops[0].unitPrice && (
-                  <span className="mt-4">
-                    {product.shops.length > 1 && "от "}
-                    {formatPrice(product.shops[0].unitPrice)} ₽ за {uom}
-                  </span>
-                )}
-                <div className="grow" />
-                {product.ean && <span className="mt-4">Арт. {product.ean}</span>}
-              </div>
-            </div>
+    <div className="pl-4 pt-4 relative product-list-item">
+      <div className="flex">
+        <div className="mr-4 rounded-2xl w-16 h-16 shrink-0 relative">
+          {product.photoUrl && (
+            <img
+              className={clsx(
+                "absolute inset-0 max-h-full m-auto",
+                styles.image,
+              )}
+              src={product.photoUrl}
+              alt="Photo"
+            />
           )}
         </div>
-        <div className={cx("w-full mt-4", styles.separator)} />
+        <div className="grow min-h-16">
+          <div className="mr-4">
+            <Link href={`/product/${product.productId}`}>
+              <a className={cx("inline-block", styles.title, styles.link)}>{insertNbspIntoName(product.name)}</a>
+            </Link>
+            {/*<div className="mt-3">*/}
+            {/*  {product.shops.map((price) => (*/}
+            {/*    <div className="mt-3" key={price.shopType}>*/}
+            {/*      <div className="flex">*/}
+            {/*        <div className="">*/}
+            {/*          <BigPrice price={price.price} />*/}
+            {/*          <div className="mt-1 flex flex-wrap justify-end text-secondary text-xs">*/}
+            {/*            {price.unitPrice && (*/}
+            {/*              <span>*/}
+            {/*                {formatPrice(price.unitPrice)} ₽ за {uom}*/}
+            {/*              </span>*/}
+            {/*            )}*/}
+            {/*          </div>*/}
+            {/*        </div>*/}
+            {/*        <div className="grow" />*/}
+            {/*        <ShopLogo shopType={price.shopType} className="h-8" monochrome />*/}
+            {/*      </div>*/}
+            {/*    </div>*/}
+            {/*  ))}*/}
+            {/*</div>*/}
+            {/*<div className="mt-3">*/}
+            {/*  {product.shops.map((price) => (*/}
+            {/*    <div className="mt-1" key={price.shopType}>*/}
+            {/*      <div className="flex items-center">*/}
+            {/*        <div className="mr-4">*/}
+            {/*          <BigPrice price={price.price} />*/}
+            {/*        </div>*/}
+            {/*        <div className="mt-1 flex flex-wrap justify-end text-secondary text-xs">*/}
+            {/*          {price.unitPrice && (*/}
+            {/*            <span>*/}
+            {/*              {formatPrice(price.unitPrice)} ₽ за {uom}*/}
+            {/*            </span>*/}
+            {/*          )}*/}
+            {/*        </div>*/}
+
+            {/*        <div className="grow" />*/}
+            {/*        <ShopLogo shopType={price.shopType} className="h-8" monochrome />*/}
+            {/*      </div>*/}
+            {/*    </div>*/}
+            {/*  ))}*/}
+            {/*</div>*/}
+            {product.shops.length > 0 && (
+              <div className="mt-4">
+                <div className="flex items-center">
+                  <BigPrice isMulti={product.shops.length > 1} price={product.shops[0].price} />
+                  <div className="grow" />
+                  <ShopLogo shopType={product.shops[0].shopType} className="h-8" monochrome />
+                </div>
+                <div className="flex flex-wrap justify-end text-secondary text-xs">
+                  {product.shops[0].unitPrice && (
+                    <span className="mt-4">
+                      {product.shops.length > 1 && "от "}
+                      {formatPrice(product.shops[0].unitPrice)} ₽ за {uom}
+                    </span>
+                  )}
+                  <div className="grow" />
+                  {product.ean && <span className="mt-4">Арт. {product.ean}</span>}
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
+      <div className={cx("w-full mt-4 ml-20", styles.separator)} />
     </div>
   )
 }
