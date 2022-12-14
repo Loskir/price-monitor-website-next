f25e80f03501e5b9bc21538a3f8c72163e81c0ff
Loskir <kl@sinker.ru>
2022-12-14 17:25:15 +0300

    Update ProductListItem

--- components/ProductListItem.tsx
+++ components/ProductListItem.tsx
@@ -4,19 +4,20 @@ import clsx from "clsx"
 import Link from "next/link"
 import React from "react"
 import { formatUom, getUpdatedAt, splitPrice } from "../functions/products"
-import { ProductPriceModel, ProductWithPriceModel, ShopType } from "../models/Product"
+import { insertNbspIntoName } from "../functions/utils"
+import { ProductPriceModel, ProductWithPriceModel } from "../models/Product"
+import { ShopLogo } from "./Logos"
 import styles from "./ProductListItem.module.css"
-import { ShopIcon } from "./shops"
 
-const ShopPriceRow: React.FC<{ price: ProductPriceModel; shopType: ShopType; uom: string }> = (
+const ShopPriceRow: React.FC<{ price: ProductPriceModel; shopType: string; uom: string }> = (
   { price, shopType, uom },
 ) => {
   const isDiscount = price.price !== price.basePrice
   const [priceWhole, priceDecimal] = splitPrice(price.price)
   const { isOutdated, dateString } = getUpdatedAt(price.time)
   return (
-    <div className="flex items-baseline text-sm font-medium flex-wrap gap-x-2">
-      <ShopIcon shopType={shopType} className="w-8 self-center" />
+    <div className="flex items-start flex-col text-sm font-medium">
+      <ShopLogo shopType={shopType} className="h-6 w-12 mb-0.5" />
       <Tooltip
         title={dateString}
         arrow
@@ -24,17 +25,23 @@ const ShopPriceRow: React.FC<{ price: ProductPriceModel; shopType: ShopType; uom
         enterTouchDelay={250}
         placement="right"
       >
-        <span className={cx("font-bold text-xl leading-tight cursor-default z-10", isOutdated && "text-gray-400")}>
+        <span
+          className={cx(
+            "font-semibold leading-tight cursor-default z-10 p-0.5 -mx-0.5 rounded",
+            isDiscount && !isOutdated && "bg-green-100",
+            isOutdated && "text-gray-400",
+          )}
+        >
           <span className="">{priceWhole}</span>
-          <span className="text-sm align-text-top">{priceDecimal}</span>
+          <span className="align-text-top" style={{ fontSize: "0.7em" }}>{priceDecimal}</span>
         </span>
       </Tooltip>
-      {isDiscount && <span className="line-through leading-tight text-gray-500">{price.basePrice}₽</span>}
-      {price.unitPrice && (
-        <span className="ml-1 leading-tight text-gray-500 text-right ml-auto">
-          {price.unitPrice.toFixed(2)}₽/{uom}
-        </span>
-      )}
+      {/*{isDiscount && <span className="line-through leading-tight text-gray-500">{price.basePrice}₽</span>}*/}
+      {/*{price.unitPrice && (*/}
+      {/*  <span className="ml-1 leading-tight text-gray-500 text-right ml-auto">*/}
+      {/*    {price.unitPrice.toFixed(2)}₽/{uom}*/}
+      {/*  </span>*/}
+      {/*)}*/}
     </div>
   )
 }
@@ -63,17 +70,18 @@ export const ProductListItem: React.FC<{ product: ProductWithPriceModel }> = ({ 
         </div>
       )}
       <div className="flex flex-col justify-center py-2 border-b border-gray-100 border-solid flex-grow">
-        <h1 className="font-semibold leading-tight mb-1">
+        <h1 className="font-semibold leading-tight mb-2">
           <Link href={`/product/${product.productId}`}>
             <a className={styles.link}>
-              {product.name}
+              {insertNbspIntoName(product.name)}
             </a>
           </Link>
         </h1>
-        {product.shops.map((shop) => (
-          <ShopPriceRow price={shop} shopType={shop.shopType} uom={uom} key={shop.shopType} />
-        ))}
-        {product.price && <ShopPriceRow price={product.price} shopType={"globus"} uom={uom} />}
+        <div className="flex flex-row gap-4">
+          {product.shops.map((shop) => (
+            <ShopPriceRow price={shop} shopType={shop.shopType} uom={uom} key={shop.shopType} />
+          ))}
+        </div>
       </div>
     </div>
   )
