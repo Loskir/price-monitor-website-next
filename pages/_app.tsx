10033aa455238a3276eb5a4e4ad1ba5bef7f0fc5
Loskir <kl@sinker.ru>
2022-12-18 16:03:13 +0300

    Add manifest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,6 +22,15 @@ function MyApp({ Component, pageProps }: AppProps) {
     <HeaderLayout>
       <Head>
         <title>Price Monitor</title>
+        <meta name="application-name" content="Price Monitor" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Price Monitor" />
+        <meta name="description" content="Поиск выгодных цен на продукты" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#FFFFFF" />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <Component {...pageProps} />
     </HeaderLayout>
