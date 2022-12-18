975e219522affcc82e7240752293ffc5dc5ad02f
Loskir <kl@sinker.ru>
2022-12-18 15:06:47 +0300

    Try PWA

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 const { withEffectorReactAliases } = require("effector-next/tools")
-// const CopyPlugin = require("copy-webpack-plugin")
+const withPWA = require("next-pwa")({
+  dest: "public",
+})
 
 const enhance = withEffectorReactAliases()
 
@@ -7,32 +9,6 @@ const enhance = withEffectorReactAliases()
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // webpack(config, { isServer }) {
-  //   // Copy zbar.wasm to where the chunk containing zbar-wasm is located
-  //   config.plugins.push(
-  //     new CopyPlugin({
-  //       patterns: [
-  //         {
-  //           // In this project, zbar-wasm becomes part of static/chunks/pages/_app.js
-  //           from: "node_modules/@undecaf/zbar-wasm/dist/zbar.wasm",
-  //           to: "static/chunks/pages/",
-  //         },
-  //       ],
-  //     }),
-  //   )
-  //
-  //   if (!isServer) {
-  //     // Resolve node modules that are irrelevant for the client
-  //     config.resolve.fallback = {
-  //       ...config.resolve.fallback,
-  //
-  //       fs: false,
-  //       path: false,
-  //     }
-  //   }
-  //
-  //   return config
-  // },
 }
 
-module.exports = enhance(nextConfig)
+module.exports = withPWA(enhance(nextConfig))
