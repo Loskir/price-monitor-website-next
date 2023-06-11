f33a1f91a35f8d52d785ebc768c43df2db591e72
Kirill Loskutov <kl@sinker.ru>
2023-06-12 02:05:07 +0300

    Refactor style to css-in-js

--- components/HeaderLayout.tsx
+++ components/HeaderLayout.tsx
@@ -1,3 +1,4 @@
+import { css } from "@emotion/css"
 import React, { ReactNode } from "react"
 import { Header } from "./Header"
 
@@ -10,4 +11,4 @@ export const HeaderLayout: React.FC<{ children: ReactNode }> = ({ children }) =>
   )
 }
 
-export const HeaderSpacer: React.FC = () => <div style={{ height: "calc(56px + env(safe-area-inset-top))" }} />
+export const HeaderSpacer: React.FC = () => <div className={css({ height: "calc(56px + env(safe-area-inset-top))" })} />
