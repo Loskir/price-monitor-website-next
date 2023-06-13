c129ef077db160201aa44f9839be220023422fbe
Kirill Loskutov <kl@sinker.ru>
2023-06-14 02:25:09 +0300

    Update nbsp thing

--- functions/utils.ts
+++ functions/utils.ts
@@ -1,7 +1,7 @@
 export const isNotNull = <T>(v: T | null): v is T => v !== null
 
 export const insertNbspIntoName = (name: string) => {
-  return name.replace(/(?:(?<=^|\s)(и|с|из|\d+)) /gi, "$1\u00A0")
+  return name.replace(/(?<=^|\s|,)(и|с|из|\d+) /gi, "$1\u00A0")
 }
 
 export const createArray = (length: number) => Array(length).fill("")
