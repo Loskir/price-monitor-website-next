f1cd3df14e092935df8ebb6087d35f73215da798
Kirill Loskutov <kl@sinker.ru>
2023-06-18 00:51:13 +0300

    Support textarea input

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,22 +1,41 @@
-import { css } from "@emotion/css"
 import clsx from "clsx"
-import React from "react"
+import React, { useCallback, useMemo } from "react"
+import TextareaAutosize from "react-textarea-autosize"
 
-export interface InputProps extends React.HTMLProps<HTMLInputElement> {
+export interface InputProps extends
+  Pick<
+    React.HTMLProps<HTMLInputElement>,
+    "className" | "value" | "placeholder"
+  >
+{
+  type?: "input" | "textarea"
+  onChange?: (value: string) => void
 }
 
 export const Input: React.FC<InputProps> = ({
-  className,
-  ...props
+  className: classNameProp,
+  type = "input",
+  onChange: onChangeProp,
+  ...forwardedProps
 }) => {
-  return (
-    <input
-      className={clsx(
-        "bg-white rounded-xl border border-gray-200 hover:border-gray-300 active:border-gray-300",
-        css`padding: 9px 19px`,
-        className,
-      )}
-      {...props}
-    />
-  )
+  const className = useMemo(() =>
+    clsx(
+      "bg-white rounded-xl px-4 py-2 border border-gray-200 hover:border-gray-300 active:border-gray-300",
+      classNameProp,
+    ), [classNameProp])
+  const onChange = useCallback((event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    onChangeProp?.(event.target.value)
+  }, [onChangeProp])
+
+  const props = {
+    className,
+    onChange,
+    ...forwardedProps,
+  }
+
+  if (type === "input") {
+    return <input {...props} />
+  }
+
+  return <TextareaAutosize {...props} />
 }
