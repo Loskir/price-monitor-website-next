34c637a493537ed99925c35c43bf9c9ed05d7248
Kirill Loskutov <kl@sinker.ru>
2023-06-21 18:10:33 +0300

    Replace css with plain style

--- components/NavbarLayout.tsx
+++ components/NavbarLayout.tsx
@@ -1,4 +1,3 @@
-import { css } from "@emotion/css"
 import React, { ReactNode } from "react"
 import { Navbar } from "./Navbar/Navbar"
 
@@ -11,4 +10,4 @@ export const NavbarLayout: React.FC<{ children: ReactNode }> = ({ children }) =>
   )
 }
 
-export const NavbarSpacer: React.FC = () => <div className={css({ height: "calc(56px + env(safe-area-inset-top))" })} />
+export const NavbarSpacer: React.FC = () => <div style={{ height: "calc(56px + env(safe-area-inset-top))" }} />
