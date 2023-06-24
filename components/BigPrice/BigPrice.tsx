87ad25d71f6011187b4bd2c0e8232dca98b9e21d
Kirill Loskutov <kl@sinker.ru>
2023-06-24 15:08:48 +0300

    Refactor styles for BigPrice

--- components/BigPrice/BigPrice.tsx
+++ components/BigPrice/BigPrice.tsx
@@ -1,17 +1,22 @@
+import { css } from "@emotion/css"
+import clsx from "clsx"
 import React from "react"
 import { splitPrice } from "../../functions/products"
 
-export const BigPrice: React.FC<{ isMulti?: boolean; price: number }> = ({
+export const BigPrice: React.FC<{ isMulti?: boolean; price: number; className?: string }> = ({
   isMulti = false,
   price,
+  className,
 }) => {
   const [priceWhole, priceDecimal] = splitPrice(price)
   return (
-    <span>
-      {isMulti && "от "}
-      <span className="text-2xl">
+    <span className={clsx("text-2xl", className)}>
+      {isMulti && <span className={css`font-size: 0.66em`}>от{" "}</span>}
+      <span>
         {priceWhole}
-        <span className="text-xs align-super ml-0.5">{priceDecimal}</span>
+        <span className={clsx("align-super", css`font-size: 0.5em; margin-left: 0.16666em`)}>
+          {priceDecimal}
+        </span>
       </span>
     </span>
   )
